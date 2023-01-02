0067111f0ee77ad90549560feb4a47c14920aeed
takumi-kuwamoto98 <takumi.kuwamoto98@gmail.com>
2023-01-02 13:54:59 +0900

    creating-dummy-data: created migration factory

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->realText(rand(15,40)),
+            'is_done' => $this->faker->boolean(10),
+            'created_at' => now(),
+            'updated_at' => now()
+        ];
+    }
+}
